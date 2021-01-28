fe0bd8ba44404b1a11e124aee449121af376ce89
progriro <progriro@gmail.com>
2021-01-28 14:42:03 +0900

    [modified] snowpack.config.js

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,19 +3,25 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  mount: {
-    /* ... */
-  },
-  plugins: [
-    /* ... */
-  ],
-  packageOptions: {
-    /* ... */
+  exclude: ["**/node_modules/**/*"],
+  alias: {
+    "@": "./src",
   },
   devOptions: {
-    /* ... */
+    secure: true,
+    open: "chrome",
+    output: "dashboard",
+    hmr: true,
+    hmrDelay: 0,
+    out: "build",
   },
   buildOptions: {
-    /* ... */
+    out: "build",
+    sourcemap: true, // Experimental
+    watch: true,
+    htmlFragments: true,
+  },
+  testOptions: {
+    files: ["__tests__/**/*", "**/*.@(spec|test).*"],
   },
 };
